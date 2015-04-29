70793e7f718e026a551e74f4e0f2a0854060ca9d
David Martínez <gollumdeagol@gmail.com>
2015-04-29 16:54:32 +0200

    first work on the buckets problem

--- buckets/buckets.pl
+++ buckets/buckets.pl
@@ -1,4 +1,24 @@
-step(CurrentState, NextState). %TODO implement this method
+/*
+ * 
+ */
+
+/* PARAMETERS */
+smallBucket(5). %capacity of the small bucket
+bigBucket(8).   %capacity of the big bucket
+goalVolume(4).  %final volume within the big bucket
+
+/* SOLVER */
+
+step([_,_], [_,0]). %empty big
+step([_,_], [0,_]). %empty small
+step([_,B], [_,NB]):- %fill big
+  bigBucket(NB),
+  B < NB.
+step([S,_], [NS,_]):- %fill small
+  smallBucket(NS),
+  S < NS.
+step([S,B],[NS,NB]):- %small -> big
+  %TODO
 
 path(State,State, Path,Path).
 path(CurrentState, GoalState, CurrentPath, TotalPath):-
@@ -7,7 +27,13 @@ path(CurrentState, GoalState, CurrentPath, TotalPath):-
   path(NextState, GoalState, [NextState|CurrentPath], TotalPath).
 
 optimalSolution:-
+ %parameters
+  smallBucket(S),
+  bigBucket(B),
+  goalVolume(G),
+  
+ %solver
   nat(N),
-  path([],[],[[]],P), %TODO fill the initial and goal states and the initial path
+  path([0,0],[0,G],[[0,0]],P), %TODO fill the initial and goal states and the initial path
   length(P,N),
   write(P).
