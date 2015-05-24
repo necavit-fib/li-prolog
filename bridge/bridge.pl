46d973964411e41a1c50aa377a1084a1a7dd3abb
David Martínez <gollumdeagol@gmail.com>
2015-05-24 10:23:19 +0200

    working on the bridge problem

--- bridge/bridge.pl
+++ bridge/bridge.pl
@@ -1,13 +1,54 @@
-step(CurrentState, NextState). %TODO implement this method
+/* **** **** **** **** **** */
+/* David Martínez Rodríguez */
+/* LI - May, 2015           */
+/* MIT License              */
+/* **** **** **** **** **** */
 
-path(State,State, Path,Path).
-path(CurrentState, GoalState, CurrentPath, TotalPath):-
-  step(CurrentState, NextState),
-  \+member(NextState, CurrentPath),
-  path(NextState, GoalState, [NextState|CurrentPath], TotalPath).
+/* *************************************************************** */
+/*                      BRIDGE PROBLEM SOLVER                      */
+/* *************************************************************** */
 
-optimalSolution:-
-  nat(N),
-  path([],[],[[]],P), %TODO fill the initial and goal states and the initial path
-  length(P,N),
-  write(P).
+/*
+ * The state is modeled as follows:
+ *   State = [<lantern position>, <left side people>, <right side people>, <cost>], where
+ *   TODO
+ */
+
+/* PARAMETERS */
+people([1,2,5,8]).  %list of people and their crossing times
+bridgeCapacity(2).  %capacity of the bridge
+
+/* SOLVER */
+solve:-
+  people(P),
+  optimalSolution([left,P,[],0],[right,[],P,_]).
+
+%FIXME
+cost([[right,_,_,C]|_],C). %the cost has been accumulated during the path construction
+cost(_,_).
+
+msubset([], []).
+msubset([E|Tail], [E|NTail]):-
+  msubset(Tail, NTail).
+msubset([_|Tail], NTail):-
+  msubset(Tail, NTail).
+
+crossBridge(PeopleOrigin, PeopleDest, Cost, PeopleOriginEnd, PeopleDestEnd, CostEnd):-
+  bridgeCapacity(Max),
+  between(1,Max,K),
+  msubset(PeopleOrigin,Group),
+  length(Group,K),
+  subtract(PeopleOrigin,Group,PeopleOriginEnd),
+  append(PeopleDest,Group,PeopleDestEnd),
+  max_list(Group,Slowest),
+  CostEnd is Cost + Slowest.
+
+/* STEPS */
+%cross bridge from LEFT to RIGHT
+step([left,Left,Right,Cost],[right,LeftEnd,RightEnd,CostEnd]):-
+  crossBridge(Left,Right,Cost, LeftEnd,RightEnd,CostEnd).
+
+%cross bridge from RIGHT to LEFT
+step([right,Left,Right,Cost],[left,LeftEnd,RightEnd,CostEnd]):-
+  crossBridge(Right,Left,Cost, RightEnd,LeftEnd,CostEnd).
+/* *************************************************************** */
